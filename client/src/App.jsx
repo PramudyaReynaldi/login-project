7ca50f8e3e298995133b2bdb0212af6b0edecb99
PramudyaReynaldi <prmdy02@gmail.com>
2023-06-07 02:35:26 +0700

    add: import Quote and adding Quote Route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import Login from './pages/Login'
 import Register from './pages/Register'
+import Quote from './pages/Quote'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Login />} />
           <Route path='/register' element={<Register />} />
+          <Route path='/quote' element={<Quote />} />
         </Routes>
       </Router>
     </>
