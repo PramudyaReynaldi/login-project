df7d05555a90dfdca92d2b8e25c05a0f62d058a4
PramudyaReynaldi <prmdy02@gmail.com>
2023-06-07 02:37:13 +0700

    add: name in User

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
 const User = new mongoose.Schema({
+    name : {
+        type: 'string',
+        required: true
+    },
     email : {
         type: 'string',
         required: true,
